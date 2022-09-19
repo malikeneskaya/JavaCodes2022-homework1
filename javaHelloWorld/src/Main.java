fb1c0e409dc03af344c25f8b6936cdc0eb72bfac
M.Enes <39553334+malikeneskaya@users.noreply.github.com>
2022-09-19 19:12:25 +0300

    Add files via upload

---
+++ javaHelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args)   {
+        // hey,i've just started java.
+        System.out.println("Hello Java!");
+        System.out.println("just started java");
+    }
+}
