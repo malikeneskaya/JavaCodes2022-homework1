285f4031739f5ee14d67adc460211d46899788e4
M.Enes <39553334+malikeneskaya@users.noreply.github.com>
2022-09-20 20:50:19 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //case sensitive e.g. System
+        //camel casing   e.g. ogrenciSayisi
+        int ogrenciSayisi = 13;
+        String mesaj = "Ogrenci Sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Ogrenci Sayim : "+ogrenciSayisi);
+        System.out.println("Ogrenci Sayim : "+ogrenciSayisi);
+    }
+}
