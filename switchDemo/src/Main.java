d6e2baedca08d1c7ae1a2acc4df9d0d5ffd7cfc0
M.Enes <39553334+malikeneskaya@users.noreply.github.com>
2022-09-21 18:38:15 +0300

    Add files via upload

---
+++ switchDemo/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char grade = 'C';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mukemmel : Gectiniz");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Iyi : Gectiniz");
+                break;
+            case 'D':
+                System.out.println("Fena Degil : Gectiniz");
+                break;
+            case 'F':
+                System.out.println("Maalesef : Kaldiniz");
+                break;
+            default:
+                System.out.println("Gecersiz Not Girdiniz");
+        }
+    }
+}
