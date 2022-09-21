91876a6bdaa95169c8e6762399c69148e2277b4f
M.Enes <39553334+malikeneskaya@users.noreply.github.com>
2022-09-21 17:11:34 +0300

    Add files via upload

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi1 = 12;
+        double sayi2 = 14.5;
+         sayi2 = -129;
+         char karakter = 'A';  //ascii codes
+         boolean dogruMu = false;
+
+    }
+}
